7b2994e18880c7f122038f47466f96ca56122e93
Daniil Shupikov <dashpkv@gmail.com>
2021-09-03 06:13:27 +0300

    Added template for room description in rooms.json

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
         print('Current position:', player.position)
         print('Rooms to go:', ROOMS_LAYOUT[player.position])
         read_key = int(input('Where to go? (1,2,3,4...)'))
-        target_room_name = ROOMS_LAYOUT[player.position][read_key-1]
+        target_room_name = ROOMS_LAYOUT[player.position][read_key - 1]
         player.walk(target_room_name, ROOMS_LAYOUT)
 
 
