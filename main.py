d39181ec17f3a3bd3f98aaaf4c8e0f7a47d621c6
Daniil Shupikov <dashpkv@gmail.com>
2024-02-02 03:30:01 +0300

    reworked main game loop and Rooms deserializing

--- main.py
+++ main.py
@@ -9,25 +9,42 @@ def read_rooms_json():
     return layout
 
 
-ROOMS_LAYOUT = read_rooms_json()
-
-
 def main():
+    rooms_dict = read_rooms_json()
+    ROOMS = {}
+    for room_id in rooms_dict:
+        ROOMS[room_id] = Room(
+            room_id,
+            rooms_dict[room_id]['name'],
+            rooms_dict[room_id]['description'],
+            rooms_dict[room_id]['exits'],
+            rooms_dict[room_id].get('requires_key', False)
+        )
+    for room_id in ROOMS:
+        exits = [ROOMS[exit_id] for exit_id in ROOMS[room_id].exits_list]
+        ROOMS[room_id].init_exits(exits)
 
-    ROOMS = []
-    for room in ROOMS_LAYOUT.keys():
-        ROOMS.append(Room(room, ROOMS_LAYOUT[room]))
-    player = Player('Daniel', ROOMS[0].name)
+    print('ROOMS: ', end='')
+    for room_id in ROOMS:
+        print(ROOMS[room_id].id_, end=', ')
+    print()
+
+    player = Player('Daniel', ROOMS['car'])
     while True:
-        print('Current position:', player.position)
-        print('Rooms to go:', ROOMS_LAYOUT[player.position])
-        read_key = int(input('Where to go? (1,2,3,4...)'))
-        target_room_name = ROOMS_LAYOUT[player.position][read_key - 1]
-        player.walk(target_room_name, ROOMS_LAYOUT)
-
-
-
+        print('Current position:', player.current_room.name)
+        print('Rooms to go: ', end='')
+        for room in player.current_room.exits:
+            print(room.id_, end=' ')
+        print()
+        room_id = input('Where do you want to go? ')
+        for room in player.current_room.exits:
+            if room_id == room.id_:
+                player.walk(room)
 
 
 if __name__ == '__main__':
     main()
+
+
+
+# TODO: map on the wall shows ASCII art of house plan
