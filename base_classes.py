8ed224d6546c0abb25a7d0b00fd4aac44580dd90
Daniil Shupikov <dashpkv@gmail.com>
2024-02-02 03:29:16 +0300

    added new properties to Room and Player

--- base_classes.py
+++ base_classes.py
@@ -1,36 +1,47 @@
 
 class Room:
-    def __init__(self, name, exits):
+    def __init__(self, id_: str, name: str, description: str, exits_list: list, requires_key):
+        self.id_ = id_
         self.name = name
+        self.description = description
+        self.exits_list = exits_list
+        self.requires_key = requires_key
+        self.exits = []
+
+    def init_exits(self, exits: list):
         self.exits = exits
-        self.closed = False
 
 
 class Player:
-    def __init__(self, name, position):
+    def __init__(self, name, current_room):
         self.name = name
         self.hp = 10
-        self.position = position
+        self.current_room = current_room
 
-    def walk(self, room_name, room_layout):
-        if room_name in room_layout[self.position]:
-            self.position = room_name
-        else:
-            print('No such room')
-            return False
+    def input_handler(self, command):
+        if "walk" in [cmd.lower() for cmd in command.split()]:
+            pass
 
+    def walk(self, new_room):
+        self.current_room = new_room
+        # if room_name in room_layout[self.position]:
+        #     self.position = room_name
+        # else:
+        #     print('No such room')
+        #     return False
 
 
 class Inventory:
     items = []
+
     def __init__(self):
         pass
 
     def remove_item(self):
         pass
+
     def use_item(self):
         pass
-
 
 
 class Item:
